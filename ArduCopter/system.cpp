cfadd26aeefa3f6ac5147bca1a667affbe704772
jerry <zhujialei888@live.com>
2026-06-01 10:21:37 +0800

    为 Pixhawk6C 添加外部信号响应时间测试，占用 uart2。修改 JCFH-A1 测试信号引脚名称。

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -13,21 +13,21 @@ static void failsafe_check_static()
     copter.failsafe_check();
 }
 
-#if defined(HAL_GPIO_PE7_TOGGLE_IN) && defined(HAL_GPIO_PE8_TOGGLE_OUT)
-static void pe7_toggle_pe8_isr()
+#if defined(HAL_GPIO_LATENCY_TOGGLE_IN) && defined(HAL_GPIO_LATENCY_TOGGLE_OUT)
+static void latency_toggle_isr()
 {
-    hal.gpio->toggle(HAL_GPIO_PE8_TOGGLE_OUT);
+    hal.gpio->toggle(HAL_GPIO_LATENCY_TOGGLE_OUT);
 }
 #endif
 
 void Copter::init_ardupilot()
 {
-#if defined(HAL_GPIO_PE7_TOGGLE_IN) && defined(HAL_GPIO_PE8_TOGGLE_OUT)
-    hal.gpio->pinMode(HAL_GPIO_PE8_TOGGLE_OUT, HAL_GPIO_OUTPUT);
-    hal.gpio->write(HAL_GPIO_PE8_TOGGLE_OUT, 0);
-    hal.gpio->pinMode(HAL_GPIO_PE7_TOGGLE_IN, HAL_GPIO_INPUT);
-    hal.gpio->attach_interrupt(HAL_GPIO_PE7_TOGGLE_IN,
-                               pe7_toggle_pe8_isr,
+#if defined(HAL_GPIO_LATENCY_TOGGLE_IN) && defined(HAL_GPIO_LATENCY_TOGGLE_OUT)
+    hal.gpio->pinMode(HAL_GPIO_LATENCY_TOGGLE_OUT, HAL_GPIO_OUTPUT);
+    hal.gpio->write(HAL_GPIO_LATENCY_TOGGLE_OUT, 0);
+    hal.gpio->pinMode(HAL_GPIO_LATENCY_TOGGLE_IN, HAL_GPIO_INPUT);
+    hal.gpio->attach_interrupt(HAL_GPIO_LATENCY_TOGGLE_IN,
+                               latency_toggle_isr,
                                AP_HAL::GPIO::INTERRUPT_RISING);
 #endif
 
